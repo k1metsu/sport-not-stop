c5863ea16f152996b8ac1987c31b1cf2ac1979f6
l.frolenkov <l.frolenkov@msocialproduction.com>
2023-02-27 12:51:41 +0300

    init project

---
+++ js/index.js
@@ -0,0 +1,46 @@
+const contactUs = document.getElementById('navAction')
+const canselButton = document.getElementById('cancel_btn')
+const burger = document.querySelectorAll('#nav-toggle')
+const menu = document.querySelectorAll('#nav-content')
+
+
+
+contactUs.addEventListener('click', () => {
+    document.getElementById('modal').classList.toggle('hidden')
+})
+
+canselButton.addEventListener('click', () => {
+    document.getElementById('modal').classList.toggle('hidden')
+})
+
+function toggleModal() {
+    document.getElementById('modal').classList.toggle('hidden')
+}
+
+if (burger.length && menu.length) {
+    for (let i = 0; i < burger.length; i++) {
+        burger[i].addEventListener('click', function () {
+            for (let j = 0; j < menu.length; j++) {
+                menu[j].classList.toggle('hidden');
+            }
+        });
+    }
+}
+
+new Swiper(".swiper", {
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+    autoplay: {
+        delay: 5000,
+        stopOnLastSlide: false,
+        disableOnInteraction: true
+    }
+});
