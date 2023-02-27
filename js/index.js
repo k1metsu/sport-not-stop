17c2b3abc2d5e3997991feb8572997912cf0b883
l.frolenkov <l.frolenkov@msocialproduction.com>
2023-02-27 13:30:43 +0300

    #notask #refactor #global - refactor

--- js/index.js
+++ js/index.js
@@ -1,21 +1,5 @@
-const contactUs = document.getElementById('navAction')
-const canselButton = document.getElementById('cancel_btn')
 const burger = document.querySelectorAll('#nav-toggle')
 const menu = document.querySelectorAll('#nav-content')
-
-
-
-contactUs.addEventListener('click', () => {
-    document.getElementById('modal').classList.toggle('hidden')
-})
-
-canselButton.addEventListener('click', () => {
-    document.getElementById('modal').classList.toggle('hidden')
-})
-
-function toggleModal() {
-    document.getElementById('modal').classList.toggle('hidden')
-}
 
 if (burger.length && menu.length) {
     for (let i = 0; i < burger.length; i++) {
